36dd91dfdad09050b15355c075d3bfb0907d49bc
hoangta <hoangta@ai-t.vn>
2017-11-08 14:07:21 +0700

    init

---
+++ src/main/java/auto/tbot/App.java
@@ -0,0 +1,14 @@
+package auto.tbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    		String url = "https://wex.nz/api/3/ticker/btc_usd-eth_usd-ltc_usd-nmc_usd-nvc_usd-ppc_usd-dsh_usd-bch_usd-zec_usd";
+        System.out.println( "Hello World!" );
+    }
+}
